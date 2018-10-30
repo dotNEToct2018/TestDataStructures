6a91c569fed370b0e8ec6c89655c68b64a1a138a
DanE <edwardsdan001@gmail.com>
2018-10-30 15:22:41 -0400

    initial commit

---
+++ TestDataStructures/LinkedListArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataStructures
+{
+    class LinkedListArray
+    {
+        private string[] items = new string[100];
+        private int Count = 0;
+
+        public int Length()
+        {
+            return Count;
+        }
+
+        public bool Insert(string newItem)
+        {
+            if(Count == items.Length)
+            {
+                return false;
+            }
+            items[Count] = newItem;
+            Count++;
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            if(Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsFull()
+        {
+            if (Count == items.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void PrintList()
+        {
+            int counter = 0;
+            Console.WriteLine("start of list -> ");
+            while(counter < Count)
+            {
+                Console.Write(items[counter]);
+                Console.Write(" -> ");
+                counter++;
+            }
+            Console.WriteLine("end of list");
+        }
+    }
+}
---
+++ TestDataStructures/LinkedListNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataStructures
+{
+    class LinkedListNode
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+        public void Add(string userInput)
+        {
+            Node newNode = new Node { Next = null, Data = userInput };
+            if (Count == 0)
+            {
+                Head = newNode;
+                Tail = newNode;
+                Count++;
+            }
+            else
+            {
+                //Change the tail next reference
+                Tail.Next = newNode;
+                //change the tail
+                Tail = newNode;
+                Count++;
+            }
+        }
+    }
+}
